70e0aa6fadc3e4158e6025933ba263ba0ecc938f
macbook <thanh.tc@vlu.edu.vn>
2025-12-02 14:39:27 +0700

    Add environment variable support with dotenv

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
@@ -10,9 +11,15 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// Route GET cho "/json" - serve JSON
+// Route GET cho "/json" - serve JSON với env variable
 app.get('/json', function(req, res) {
-  res.json({"message": "Hello json"});
+  let message = "Hello json";
+  
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+  
+  res.json({"message": message});
 });
 
 module.exports = app;
